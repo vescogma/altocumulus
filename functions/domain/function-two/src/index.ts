735912a60f2fac6d0d32fca10f95986d16080fa7
Miguel Vesco <vescogma@gmail.com>
2022-08-08 08:52:11 -0400

    🍷

--- functions/domain/function-two/src/index.ts
+++ functions/domain/function-two/src/index.ts
@@ -1,5 +1,7 @@
 import { cloudEvent } from '@google-cloud/functions-framework'
+import { getRandomName } from '@altocumulus/domain'
 
 cloudEvent('functionTwo', async (event) => {
+  console.log('dep test', getRandomName())
   console.log(event.id, event.type, event.source)
 })
